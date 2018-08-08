345a5b74ebf26485af523b7c7f18d332b95d277d
usjaved <us.javed@outlook.com>
2018-08-08 14:19:23 +0500

    changes

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-
+echo "Running"
 git config --global user.email "us.javed@outlook.com"
 git config --global user.name "usjaved"
 
-git checkout -b "$1"
-git merge master
-git commit -am "$CIRCLE_BRANCH build#$CIRCLE_BUILD_NUM"
-git push --force --set-upstream origin $1
+git checkout "master"
+git merge devbranch
+git push https://github.com/usjaved/AzureDeployment.git
